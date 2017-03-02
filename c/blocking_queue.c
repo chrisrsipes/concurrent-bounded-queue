88d8b55b239be2b3b50c3251d91a839af4b6f4ed
Christopher Sipes <crs@Christophers-MacBook-Pro.local>
2017-03-01 22:54:41 -0500

    Implemented custom thread pool in scheduleRequest, required addition of isComplete on requests.
    Created project in CLion.
    
    Still need to
    - change the locks to lock head / tail pointers, not entire queue
    - do cleanup in main after threads are finished.

--- c/blocking_queue.c
+++ c/blocking_queue.c
@@ -33,21 +33,115 @@ typedef struct BlockingQueueResponse {
 } BlockingQueueResponse;
 
 typedef struct BlockingQueueRequest {
-  BlockingQueueResponse *response;
+  BlockingQueueResponse response;
   BlockingQueue *queue;
   OperationType operationType;
   int data;
+  int isComplete;
+  int threadIndex;
 } BlockingQueueRequest;
 
+void initializeGlobals(int threadCount);
+
+BlockingQueueRequest *getActiveRequest(int index);
+int setActiveRequest(BlockingQueueRequest *request, int index);
+pthread_t *getActiveThread(int index);
+int setActiveThread(pthread_t *thread, int index);
+void printActiveRequests();
 BlockingQueue *createBlockingQueue(int capacity);
 BlockingQueue *destroyBlockingQueue(BlockingQueue *ptr);
-BlockingQueueResponse *createBlockingQueueResponse(ErrorType errorType, int hasError, int data);
 BlockingQueueRequest *createBlockingQueueRequest(BlockingQueue *q, OperationType op, int data);
 BlockingQueueRequest *destroyBlockingQueueRequest(BlockingQueueRequest *ptr);
 int validateRequest(BlockingQueueRequest *request);
-void *enqueue(void *input);
-void *dequeue(void *input);
+void *enqueue_implementation(void *input);
+void *dequeue_implementation(void *input);
+void scheduleRequest(BlockingQueueRequest *request);
+void enqueue(BlockingQueueRequest *request);
+void dequeue(BlockingQueueRequest *request);
 
+int THREAD_COUNT = -1;
+BlockingQueueRequest **ACTIVE_REQUESTS = NULL;
+pthread_t **ACTIVE_THREADS = NULL;
+
+void initializeGlobals(int threadCount) {
+  int i;
+
+  THREAD_COUNT = threadCount;
+  ACTIVE_REQUESTS = malloc(sizeof(BlockingQueueRequest*) * threadCount);
+
+  if (ACTIVE_REQUESTS == NULL) {
+    return;
+  }
+
+  ACTIVE_THREADS = malloc(sizeof(pthread_t*) * threadCount);
+
+  if (ACTIVE_THREADS == NULL) {
+    free(ACTIVE_REQUESTS);
+    return;
+  }
+
+  for (i = 0; i < threadCount; i++) {
+    ACTIVE_THREADS[i] = malloc(sizeof(pthread_t));
+    ACTIVE_REQUESTS[i] =  NULL;
+  }
+
+  return;
+}
+
+BlockingQueueRequest *getActiveRequest(int index) {
+  if (ACTIVE_REQUESTS == NULL)
+    return NULL;
+  else if (index == -1 || index >= THREAD_COUNT)
+    return NULL;
+  else
+    return ACTIVE_REQUESTS[index];
+}
+
+int setActiveRequest(BlockingQueueRequest *request, int index) {
+  if (ACTIVE_REQUESTS == NULL)
+    return 0;
+  else if (index == -1 || index >= THREAD_COUNT) {
+    return 0;
+  }
+  else {
+    ACTIVE_REQUESTS[index] = request;
+    return 1;
+  }
+}
+
+pthread_t *getActiveThread(int index) {
+  if (ACTIVE_THREADS == NULL)
+    return NULL;
+  else if (index == -1 || index >= THREAD_COUNT)
+    return NULL;
+  else
+    return ACTIVE_THREADS[index];
+}
+
+int setActiveThread(pthread_t *thread, int index) {
+  if (ACTIVE_THREADS == NULL)
+    return 0;
+  else if (index == -1 || index >= THREAD_COUNT) {
+    return 0;
+  }
+  else {
+    ACTIVE_THREADS[index] = thread;
+    return 1;
+  }
+}
+
+void printActiveRequests() {
+  int i;
+
+  // printf("Active Requests: [");
+
+  for (i = 0; i < THREAD_COUNT; i++) {
+    printf("%p, ", ACTIVE_REQUESTS[i]);
+  }
+
+  printf("]\n\n");
+
+}
 
 // allocates memory for a new BlockingQueue
 BlockingQueue *createBlockingQueue(int capacity) {
@@ -90,22 +184,6 @@ BlockingQueue *destroyBlockingQueue(BlockingQueue *ptr) {
   return NULL;
 }
 
-BlockingQueueResponse *createBlockingQueueResponse(ErrorType errorType, int hasError, int data) {
-
-  BlockingQueueResponse *response = NULL;
-
-  response = malloc(sizeof(BlockingQueueResponse));
-
-  if (response == NULL)
-    return NULL;
-
-  response->errorType = errorType;
-  response->hasError = hasError;
-  response->data = data;
-
-  return response;
-}
-
 BlockingQueueRequest *createBlockingQueueRequest(BlockingQueue *q, OperationType op, int data) {
   BlockingQueueRequest *request = NULL;
 
@@ -120,7 +198,11 @@ BlockingQueueRequest *createBlockingQueueRequest(BlockingQueue *q, OperationType
   request->queue = q;
   request->operationType = op;
   request->data = data;
-  request->response = createBlockingQueueResponse(NO_ERROR, 0, INT_MIN);
+  request->isComplete = 0;
+
+  request->response.errorType = NO_ERROR;
+  request->response.hasError = 0;
+  request->response.data = INT_MIN;
 
   return request;
 }
@@ -129,9 +211,6 @@ BlockingQueueRequest *destroyBlockingQueueRequest(BlockingQueueRequest *ptr) {
   if (ptr == NULL)
     return NULL;
 
-  if (ptr->response != NULL)
-    free(ptr->response);
-
   free(ptr);
 
   return NULL;
@@ -139,29 +218,27 @@ BlockingQueueRequest *destroyBlockingQueueRequest(BlockingQueueRequest *ptr) {
 
 int validateRequest(BlockingQueueRequest *request) {
 
-  if (request == NULL || request->response == NULL) {
-    return 0;
-  }
-  else if (request == NULL) {
-    request->response->hasError = 1;
-    request->response->errorType = REQUEST_NULL_EXCEPTION;
+  if (request == NULL) {
+    request->response.hasError = 1;
+    request->response.errorType = REQUEST_NULL_EXCEPTION;
     return 0;
   }
   else if (request->queue == NULL || request->queue->array == NULL) {
-    request->response->hasError = 1;
-    request->response->errorType = QUEUE_NULL_EXCEPTION;
+    request->response.hasError = 1;
+    request->response.errorType = QUEUE_NULL_EXCEPTION;
     return 0;
   }
 
   return 1;
 }
 
-void *enqueue(void *input) {
+void *enqueue_implementation(void *input) {
   BlockingQueueRequest *request = (BlockingQueueRequest*)input;
   BlockingQueue *queue = NULL;
 
   // there was an error, cancel here
   if (!validateRequest(request)) {
+    request->isComplete = 1;
     return request;
   }
 
@@ -172,33 +249,39 @@ void *enqueue(void *input) {
 
   // queue is full, unlock and return
   if (queue->tail - queue->head == queue->capacity) {
-    request->response->hasError = 1;
-    request->response->errorType = QUEUE_FULL_EXCEPTION;
+    if (DEBUG > 1) {
+      printf("- [%c] enqueue failed, queue full\n", request->threadIndex + 'A');
+    }
 
+    request->response.hasError = 1;
+    request->response.errorType = QUEUE_FULL_EXCEPTION;
     pthread_mutex_unlock(&queue->headLock);
-    return request->response;
+    request->isComplete = 1;
+
+    return request;
   }
 
-  if (DEBUG > 2) {
-    printf("- enqueueing element %d at position %d.\n", request->data, queue->tail);
-  }
-  
   queue->array[queue->tail % queue->capacity] = request->data;
   queue->tail++;
 
-  // unlock
+  if (DEBUG > 1) {
+    printf("- [%c] enqueued %d\n", request->threadIndex + 'A', request->data);
+  }
+
   pthread_mutex_unlock(&queue->headLock);
+  request->isComplete = 1;
 
   return request;
 }
 
 // removes an element from the queue and returns it
-void *dequeue(void *input) {
+void *dequeue_implementation(void *input) {
   BlockingQueueRequest *request = (BlockingQueueRequest*)input;
   BlockingQueue *queue = NULL;
   int data;
 
   if (!validateRequest(request)) {
+    request->isComplete = 1;
     return request;
   }
 
@@ -207,91 +290,132 @@ void *dequeue(void *input) {
   pthread_mutex_lock(&queue->headLock);
 
   if (queue->tail == queue->head) {
-    request->response->hasError = 1;
-    request->response->errorType = QUEUE_EMPTY_EXCEPTION;
+    if (DEBUG > 1)
+      printf("- [%c] deqeue failed, queue empty\n", request->threadIndex + 'A');
 
+    request->response.hasError = 1;
+    request->response.errorType = QUEUE_EMPTY_EXCEPTION;
     pthread_mutex_unlock(&queue->headLock);
+
+    request->isComplete = 1;
     return request;
   }
 
-  if (DEBUG > 2) {
-    printf("- dequeueing element at position %d.\n", queue->head);
+  request->response.data = queue->array[queue->head % queue->capacity];
+  queue->head++;
+  pthread_mutex_unlock(&queue->headLock);
+
+  if (DEBUG > 1) {
+    printf("- [%c] dequeued %d.\n", request->threadIndex + 'A', request->response.data);
   }
 
-  request->response->data = queue->array[queue->head % queue->capacity];
-  queue->head++;
-  
-  pthread_mutex_unlock(&queue->headLock);
+  request->isComplete = 1;
 
   return request;
 }
 
-int main(int argc, char **argv) {
-  BlockingQueue *queue = NULL; 
-  BlockingQueueRequest **requests = NULL;
-  pthread_t **threadArray = NULL;
-  OperationType operationType;
-  int i, threadCount, operationLimit, operationCount, enqueueCount;
-  int *operationList, *enqueueList;
+void scheduleRequest(BlockingQueueRequest *request) {
+  int  threadIndex = 0;
+  BlockingQueueRequest *current = NULL;
 
-  srand(time(NULL));
+  // while there's no thread available
+  while (1) {
+    current = getActiveRequest(threadIndex % THREAD_COUNT);
+
+    if (current == NULL) {
+      break;
+    }
+    else if(current->isComplete) {
+      // freeing this should be done in main after program is finished running
+      setActiveRequest(NULL, threadIndex % THREAD_COUNT);
+
+      // wait for the thread to say it's done
+      pthread_join(*getActiveThread(threadIndex % THREAD_COUNT), NULL);
+
+      break;
+    }
+
+    threadIndex++;
+  }
+
+  request->threadIndex = threadIndex % THREAD_COUNT;
+  setActiveRequest(request, threadIndex % THREAD_COUNT);
+
+  if (request->operationType == ENQUEUE) {
+    pthread_create(getActiveThread(threadIndex % THREAD_COUNT), NULL, enqueue_implementation, (void*)request);
+  }
+  else {
+    pthread_create(getActiveThread(threadIndex % THREAD_COUNT), NULL, dequeue_implementation, (void*)request);
+  }
+
+}
+
+void enqueue(BlockingQueueRequest *request) {
+  scheduleRequest(request);
+  return;
+}
+
+void dequeue(BlockingQueueRequest *request) {
+  scheduleRequest(request);
+  return;
+}
+
+int main(int argc, char **argv) {
+  int i, r, threadCount, operationLimit, operationCount;
+  BlockingQueue *queue = NULL;
+  BlockingQueueRequest **requestList;
 
   if (argc < 3) {
     printf("Invalid program input.  Please run using ./a.out <threadCount> <operationCount>\n\n");
     return -1;
   }
 
+  srand(time(NULL));
+
   threadCount = atoi(argv[1]);
   operationLimit = atoi(argv[2]);
 
+  initializeGlobals(threadCount);
+
   queue = createBlockingQueue(10); 
+  
+  requestList = malloc(sizeof(BlockingQueueRequest*) * operationLimit);
 
-  threadArray = malloc(sizeof(pthread_t*) * threadCount);
-  requests = malloc(sizeof(BlockingQueueRequest*) * threadCount);
-
-  operationList = malloc(sizeof(int) * operationLimit);
-  enqueueList = malloc(sizeof(int) * operationLimit);
+  if (DEBUG > 2)
+    printf("request list: \n[");
 
   // generate operations and elements that can be enqueued
   for (i = 0; i < operationLimit; i++) {
-    enqueueList[i] = rand() % 1000;
-    operationList[i] = enqueueList[i] % 2;
+    r = rand() % 1000;
+    requestList[i] = createBlockingQueueRequest(queue, r % 2 ? ENQUEUE : DEQUEUE, r);
+
+    if (DEBUG > 2) {
+      if (requestList[i]->operationType == ENQUEUE)
+        printf("enq(%d), ", requestList[i]->data);
+      else 
+        printf("deq(), ");
+    }
   }
 
-  // intiializes thread
-  for (i = 0; i < threadCount; i++) {
-    threadArray[i] = malloc(sizeof(pthread_t));
-  }
+  if (DEBUG > 2) 
+    printf("]\n\n");
 
-  // executes generated operations with generated values
-  operationCount = enqueueCount = 0;
+  operationCount = 0;
+
   while (operationCount < operationLimit) {
-    // 1: enqueue, 0: dequeue
-    operationType = operationList[operationCount] ? ENQUEUE : DEQUEUE;
-
-    // @TODO: need to figure out how to manage threads w/ threadpool
-    // so that they can be reused once other threads are finished executing
-    // can current only have operationLimit < threadCount
-    if (operationType == ENQUEUE) {
-      printf("enqueue %d\n", enqueueList[enqueueCount]);
-      requests[operationCount] = createBlockingQueueRequest(queue, ENQUEUE, enqueueList[enqueueCount++]);
-      pthread_create(threadArray[operationCount], NULL, enqueue, (void*)requests[operationCount]);
+    if (requestList[operationCount]->operationType == ENQUEUE) {
+      enqueue(requestList[operationCount++]);
     }
     else {
-      printf("dequeue\n");
-      requests[operationCount] = createBlockingQueueRequest(queue, DEQUEUE, INT_MIN);
-      pthread_create(threadArray[operationCount], NULL, dequeue, (void*)requests[operationCount]);
+      dequeue(requestList[operationCount++]);
     }
-
-    operationCount++;
   }
 
-  // waits for all threads to finish
-  for (i = 0; i < threadCount; i++)
-    pthread_join(*threadArray[i], NULL);
-
-  if (DEBUG > 2)
-    printf("Finished creating enqueue threads.\n");
+  // wait for remaining active requests to finish
+  for (i = 0; i < THREAD_COUNT; i++) {
+    if (getActiveThread(i) != NULL)
+      pthread_join(*getActiveThread(i), NULL);
+  }
 
   return 0;
 }
